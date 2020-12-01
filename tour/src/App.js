28d98be44fa49cf36265a78fd92f965244c36315
Rahul Shinde <rahulshinde@Rahuls-MacBook-Air.local>
2020-12-01 20:33:04 +0530

    fetch data and pass to tours components

--- tour/src/App.js
+++ tour/src/App.js
@@ -1,6 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import Loading from './component/Loading';
+import Tours from './component/Tours';
+
+const url =
+	'https://raw.githubusercontent.com/shinderahul/reactjs/main/tour/src/tours.json';
 
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+	const [tours, setTours] = useState([]);
+
+	const fetchTours = async () => {
+		setLoading(true);
+		try {
+			const response = await fetch(url);
+			const tours = await response.json();
+			setLoading(false);
+			setTours(tours);
+			// console.log(tours);
+		} catch (error) {
+			setLoading(false);
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchTours();
+	}, []);
+
+	if (loading) {
+		return (
+			<main>
+				<Loading />
+			</main>
+		);
+	}
+
+	return (
+		<main>
+			<Tours tours={tours} />
+		</main>
+	);
 }
 
 export default App;
